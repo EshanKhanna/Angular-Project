a8c2a664fc1af3d625e2a2fbf41b4eab24c3a823
EshanKhanna <khannaeshan@yahoo.in>
2023-03-15 22:58:29 +0530

    2nd commit

--- Angular Quiz App/AngularQuiz/src/app/question/question.component.ts
+++ Angular Quiz App/AngularQuiz/src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuestionService } from '../service/service/question.service';
 
 @Component({
   selector: 'app-question',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent {
+  public name: string = "";
+  public questionList : any = [];
+  public currentQuestion:number = 0;
+  public points: number=0;
+  counter=60;
+  constructor(private questionService: QuestionService) { }
+
+  ngOnInit(): void {
+    this.name = localStorage.getItem("name")!;
+    this.getAllQuestions();
+
+  }
+  getAllQuestions(){
+    this.questionService.getQuestionJson()
+    .subscribe(res =>{
+      this.questionList = res.questions;
+
+  })
+  }
+  nextQuestion(){
+    this.currentQuestion++;
+
+  }
+  previousQuestion(){
+    this.currentQuestion--;
+
+  }
+
 
 }
